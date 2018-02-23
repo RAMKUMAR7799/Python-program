4dd68e3a1108ce44f72d8edb689b8282fbd958b7
RAMKUMAR7799 <ramaselvanram@gmail.com>
2018-02-23 13:39:54 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+let = input("Enter any character: ")
+if(let=='a' or let=='A' or let=='e' or let=='E' or let=='i' or let=='I'or let=='o' or let=='O' or let=='u' or let=='U'):
+  print(let, "is a vowel.")
+else:
+  print(let, "is a consonant.")
