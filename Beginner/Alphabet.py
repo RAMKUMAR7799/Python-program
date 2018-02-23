d609ee34f8575c622bfa0f1dbc51dc00d06cf961
RAMKUMAR7799 <ramaselvanram@gmail.com>
2018-02-23 13:58:52 +0530

    Create Alphabet.py

---
+++ Beginner/Alphabet.py
@@ -0,0 +1,7 @@
+let=input("Enter Your Characator:")
+if(let>='a' and let<='z'):  
+  print(let,"is a Alphabet")
+elif(let>='A' and let<='Z' ):
+  print(let,"is a Alphabet")
+else:
+  print(let,"is No")
