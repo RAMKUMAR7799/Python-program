96b4c74f4c1e78a8159bf2d6a198485321ca797e
RAMKUMAR7799 <ramaselvanram@gmail.com>
2018-07-24 15:31:24 +0530

    Create noword.py

---
+++ Beginner/noword.py
@@ -0,0 +1,23 @@
+num=int(input("Enter the Number:"))
+if num==1:
+  print("One")
+elif num==2:
+  print("Two")
+elif num==3:
+  print("Three")
+elif num==4:
+  print("Four")
+elif num==5:
+  print("Five")
+elif num==6:
+  print("Six")
+elif num==7:
+  print("Seven")
+elif num==8:
+  print("eight")
+elif num==9:
+  print("Nine")
+elif num==10:
+  print("Ten")
+else:
+  print("Sorry Out of Range")
